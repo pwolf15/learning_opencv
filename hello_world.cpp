f55d9902f3b9d2a6f98a7cb25f71bb8ecd699663
pwolf15 <patrick.wolf@live.com>
2021-08-01 14:39:09 -0700

    CV HelloWorld

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char** argv)
+{
+  cv::Mat img = cv::imread(argv[1], -1);
+  if (img.empty())
+    return -1;
+  cv::namedWindow("Example 2-1", cv::WINDOW_AUTOSIZE);
+  cv::imshow("Example 2-1", img);
+  cv::waitKey(0);
+  cv::destroyWindow("Example 2-1");
+  return 0;
+}
