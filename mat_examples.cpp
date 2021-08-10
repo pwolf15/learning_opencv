153ba5626f44e87c5d78969a07776d3e2ff06429
pwolf15 <patrick.wolf@live.com>
2021-08-09 22:22:14 -0700

    Mat examples updates

--- mat_examples.cpp
+++ mat_examples.cpp
@@ -12,6 +12,10 @@ int main(int argc, char** argv)
 
   cv::Mat m2 = cv::Mat::eye(10,10, CV_32FC2);
 
-  std::cout << "Element (10,10) is (" << m2.at<cv::Vec2f>(3,3)[0] << "," << m2.at<cv::Vec2f>(3,3)[1] << ")" << std::endl;
+  std::cout << "Element (3,3) is (" << m2.at<cv::Vec2f>(3,3)[0] << "," << m2.at<cv::Vec2f>(3,3)[1] << ")" << std::endl;
+
+  cv::Mat m3 = cv::Mat::eye(10,10,cv::DataType<cv::Complexf>::generic_type);
+
+  std::cout << "Element (3,3) is (" << m3.at<cv::Complexf>(3,3).re << "," << m3.at<cv::Complexf>(3,3).im << ")" << std::endl;
   return 0;
 }
