6ddf4b434db513062a0b56fe5c745eec8c7ff273
pwolf15 <patrick.wolf@live.com>
2021-08-14 14:35:55 -0700

    Start to 'typewriter'

--- typewriter.cpp
+++ typewriter.cpp
@@ -1,23 +1,58 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 
+#include <iostream>
+
+void drawSquare(cv::Mat& m, int r, int c, int w, int h)
+{
+  for (int i = r; i < r + h; ++i)
+  {
+    for (int j = c; j < c + w; ++j)
+    {
+      m.at<uchar>(i,j) = 255;
+    }
+  }
+}
+
 int main()
 {
 
   cv::namedWindow("Typewriter", cv::WINDOW_AUTOSIZE);
   cv::Mat m(500, 500, CV_8UC1);
 
-  for (int i = 0; i < m.rows; ++i)
-  {
-    for (int j = 0; j < m.cols; ++j)
-    {
-      m.at<uchar>(i,j) = rand() % 255;
-    }
-  }
+  // for (int i = 0; i < m.rows; ++i)
+  // {
+  //   for (int j = 0; j < m.cols; ++j)
+  //   {
+  //     m.at<uchar>(i,j) = rand() % 255;
+  //   }
+  // }
 
-  cv::imshow("Typewriter", m);
-  
-  cv::waitKey(0);
+  int w = 10;
+  int h = 20;
+  int rowIdx = 0;
+  int colIdx = 0;
+
+  while (true)
+  {
+    cv::imshow("Typewriter", m);
+
+    char c = (char) cv::waitKey(10);
+    if (c >= 48 && c <= 57)
+    {
+      std::cout << "Draw square" << std::endl;
+      drawSquare(m, rowIdx, colIdx, w, h);
+      colIdx += w;
+      
+      if (colIdx == 500)
+      {
+        rowIdx += h;
+        colIdx = 0;
+      }
+    }
+    if (c == 27)
+      break;
+  }
 
   return 0;
 }
