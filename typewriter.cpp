380bf998fd6113a5208e189c4a580a6d257d073d
pwolf15 <patrick.wolf@live.com>
2021-08-15 16:10:24 -0700

    Added enum for KeyCodes, added CR, BKSP.
    @TODO: Cleanup cursor, advance logic

--- typewriter.cpp
+++ typewriter.cpp
@@ -16,11 +16,39 @@ void drawSquare(cv::Mat& m, int r, int c, int w, int h, int keyIdx)
   }
 }
 
+void drawBlack(cv::Mat& m, int r, int c, int w, int h)
+{
+  for (int i = r; i < r + h; ++i)
+  {
+    for (int j = c; j < c + w; ++j)
+    {
+      m.at<uchar>(i,j) = 0;
+    }
+  }
+}
+
+enum KeyCodes
+{
+  ZERO = 48,
+  NINE = KeyCodes::ZERO + 9,
+  LEFT = 81,
+  UP = 82,
+  RIGHT = 83,
+  DOWN = 84
+};
+
 int main()
 {
 
   cv::namedWindow("Typewriter", cv::WINDOW_AUTOSIZE);
-  cv::Mat m(500, 500, CV_8UC1);
+  const int windowWidth = 500;
+  const int windowHeight = 500;
+  const int w = 10;
+  const int h = 20;
+  int rowIdx = 0;
+  int colIdx = 0;
+
+  cv::Mat m(windowWidth, windowHeight, CV_8UC1);
 
   // for (int i = 0; i < m.rows; ++i)
   // {
@@ -30,30 +58,99 @@ int main()
   //   }
   // }
 
-  int w = 10;
-  int h = 20;
-  int rowIdx = 0;
-  int colIdx = 0;
+
 
   while (true)
   {
     cv::imshow("Typewriter", m);
 
     char c = (char) cv::waitKey(10);
-    if (c >= 48 && c <= 57)
+    if (c == 27)
+    {
+      break;
+    }
+    else if (colIdx >= windowWidth || rowIdx >= windowHeight)
+    {
+      continue;
+    }
+    else if (c >= KeyCodes::ZERO && c <= KeyCodes::NINE) // number
     {
       std::cout << "Draw square" << std::endl;
-      drawSquare(m, rowIdx, colIdx, w, h, c - 48);
+      drawSquare(m, rowIdx, colIdx, w, h, c - KeyCodes::ZERO);
       colIdx += w;
       
-      if (colIdx == 500)
+      if (colIdx == windowWidth)
       {
         rowIdx += h;
         colIdx = 0;
       }
     }
-    if (c == 27)
-      break;
+    else if (c == 13) // carriage return
+    {
+      rowIdx += h;
+      colIdx = 0;
+    }
+    else if (c == 8) // backspace
+    {
+      drawBlack(m, rowIdx, colIdx, w, h);
+      if (colIdx == 0)
+      {
+        if (rowIdx != 0)
+        {
+          rowIdx -= h;
+        }
+      }
+      else 
+      {
+        colIdx -= w;
+      }
+    }
+    else if (c == KeyCodes::LEFT) // l
+    {
+      std::cout << "LEFT" << std::endl;
+      if (colIdx == 0)
+      {
+        if (rowIdx != 0)
+        {
+          rowIdx -= h;
+        }
+      }
+      else
+      {
+        colIdx -= w;
+      }
+    }
+    else if (c == KeyCodes::UP) // u
+    {
+          std::cout << "UP" << std::endl;
+      if (rowIdx != 0)
+      {
+        rowIdx -= h;
+      }
+    }
+    else if (c == KeyCodes::RIGHT) // r
+    {
+      std::cout << "RIGHT" << std::endl;
+      if (colIdx == windowWidth)
+      {
+        colIdx = 0;
+        rowIdx += h; 
+      }
+      else 
+      {
+        colIdx += w;
+      }
+      
+    }
+    else if (c == KeyCodes::DOWN) // d
+    {
+      std::cout << "DOWN" << std::endl;
+      if (rowIdx != windowHeight)
+      {
+        rowIdx += h;
+      }
+    }
+
   }
 
   return 0;
