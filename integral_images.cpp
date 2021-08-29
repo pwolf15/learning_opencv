0d3583d4b0b6b9d1ca5b9dc78ae223101e897d34
pwolf15 <patrick.wolf@live.com>
2021-08-29 09:21:57 -0700

    Added timing for integral images method

--- integral_images.cpp
+++ integral_images.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 
+#include <chrono>
 #include <iostream>
 
 enum KeyCodes
@@ -23,7 +24,7 @@ void randomize_image(cv::Mat& m, int w, int h)
   {
     for (int j = 0; j < w; ++j)
     {
-      m.at<uchar>(i,j) = rand() % 256;
+      m.at<uchar>(i, j) = rand() % 256;
     }
   }
 }
@@ -39,10 +40,10 @@ void generate_integral_image(cv::Mat& mI, cv::Mat m, int w, int h)
       {
         for (int l = 0; l <= j; ++l)
         {
-          pixelValue += (float)m.at<uchar>(k,l);
+          pixelValue += (float)m.at<uchar>(k, l);
         }
       }
-      mI.at<float>(i,j) = pixelValue;
+      mI.at<float>(i, j) = pixelValue;
     }
   }
 }
@@ -59,7 +60,7 @@ void generate_integral_image2(cv::Mat& mI, cv::Mat m, int w, int h)
         if (j != 0)
         {
           // add left pixel
-          pixelValue += mI.at<float>(i,j - 1);
+          pixelValue += mI.at<float>(i, j - 1);
         }
       }
       else if (j == 0)
@@ -67,18 +68,18 @@ void generate_integral_image2(cv::Mat& mI, cv::Mat m, int w, int h)
         if (i != 0)
         {
           // add top pixel
-          pixelValue += mI.at<float>(i - 1,j);
+          pixelValue += mI.at<float>(i - 1, j);
         }
       }
       else
       {
-        pixelValue += mI.at<float>(i,j - 1);
-        pixelValue += mI.at<float>(i - 1,j);
-        pixelValue -= mI.at<float>(i - 1,j - 1);
+        pixelValue += mI.at<float>(i, j - 1);
+        pixelValue += mI.at<float>(i - 1, j);
+        pixelValue -= mI.at<float>(i - 1, j - 1);
       }
 
-      pixelValue += (float)m.at<uchar>(i,j);
-      mI.at<float>(i,j) = pixelValue;
+      pixelValue += (float)m.at<uchar>(i, j);
+      mI.at<float>(i, j) = pixelValue;
     }
   }
 }
@@ -90,7 +91,7 @@ int max_val(cv::Mat m, int w, int h)
   {
     for (int j = 0; j < w; ++j)
     {
-      maxVal = std::max(maxVal, m.at<float>(i,j));
+      maxVal = std::max(maxVal, m.at<float>(i, j));
     }
   }
 
@@ -103,7 +104,7 @@ void scale_image(cv::Mat& m, int w, int h, int maxVal)
   {
     for (int j = 0; j < w; ++j)
     {
-      m.at<float>(i,j) = m.at<float>(i, j) / maxVal;
+      m.at<float>(i, j) = m.at<float>(i, j) / maxVal;
     }
   }
 }
@@ -128,16 +129,37 @@ int main()
   randomize_image(m, w, h);
 
   // method 1: loop over every pixel
+
+  auto start = std::chrono::steady_clock::now();
+
   generate_integral_image(mI, m, w, h);
+
+  auto finish = std::chrono::steady_clock::now();
+  double elapsed_seconds =
+      std::chrono::duration_cast<std::chrono::duration<double>>(finish - start)
+          .count();
+
+  std::cout << "Method 1: " << elapsed_seconds << std::endl;
+
   // method 2: efficient a (left) + b (top) - c (top left for double count)
+  
+  start = std::chrono::steady_clock::now();
+
   generate_integral_image2(mI2, m, w, h);
+
+  finish = std::chrono::steady_clock::now();
+  elapsed_seconds =
+      std::chrono::duration_cast<std::chrono::duration<double>>(finish - start)
+          .count();
+
+  std::cout << "Method 2: " << elapsed_seconds << std::endl;
 
   // check equal
   for (int i = 0; i < h; ++i)
   {
     for (int j = 0; j < w; ++j)
     {
-      assert(mI.at<float>(i,j) == mI2.at<float>(i,j));
+      assert(mI.at<float>(i, j) == mI2.at<float>(i, j));
     }
   }
 
@@ -146,7 +168,7 @@ int main()
   maxVal = max_val(mI2, w, h);
   scale_image(mI2, w, h, maxVal);
 
-  while(true)
+  while (true)
   {
     cv::imshow("Image", m);
     cv::imshow("Integral image", mI);
