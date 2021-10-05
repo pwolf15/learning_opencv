5238ccf8d11b10eb1d7ec0fdbaca8430cca091fd
pwolf15 <patrick.wolf@live.com>
2021-10-04 22:12:22 -0700

    Added 'pyramid' with masking functionality

--- ch05/pyramid.cpp
+++ ch05/pyramid.cpp
@@ -1,27 +1,18 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 
-int drawLevel(cv::Mat& m, int idx)
-{
-  int offset = idx * 10;
-  for (int i = offset; i < 210 - offset; ++i)
-  {
-    for (int j = offset; j < 210 - offset; ++j)
-    {
-      m.at<uchar>(i,j) = idx * 20;
-    }
-  }
-}
-
 int main(int argc, char** argv)
 {
   cv::namedWindow("pyramid", cv::WINDOW_AUTOSIZE);
 
-  cv::Mat m(210, 210, CV_8UC1, cv::Scalar(0));
+  int width = 210, height = 210, numLevels = 10;
+  cv::Mat m(height, width, CV_8UC1, cv::Scalar(0));
 
-  for (int i = 1; i <= 10; ++i)
+  for (int i = 1; i <= numLevels; ++i)
   {
-    drawLevel(m, i);
+    cv::Mat mask = cv::Mat::zeros(width, height, CV_8U); 
+    mask(cv::Rect(i * 10, i * 10, width - (i * 10 * 2), height - (i * 10 * 2))) = 1;
+    m.setTo(cv::Scalar(i * 20), mask);
   }
 
   cv::imshow("pyramid", m);
