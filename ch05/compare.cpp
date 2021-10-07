c915d27d917a6e53c1d85a0eec465dd45133dc9e
pwolf15 <patrick.wolf@live.com>
2021-10-06 22:15:31 -0700

    Added 'compare' example fixes

--- ch05/compare.cpp
+++ ch05/compare.cpp
@@ -3,11 +3,10 @@
 
 int main(int argc, char** argv)
 {
-  cv::namedWindow("red", cv::WINDOW_AUTOSIZE);
   cv::namedWindow("green", cv::WINDOW_AUTOSIZE);
-  cv::namedWindow("blue", cv::WINDOW_AUTOSIZE);
+  cv::namedWindow("green_compare", cv::WINDOW_AUTOSIZE);
 
-  cv::Mat img = cv::imread("/home/pwolf/Downloads/IMG_3875.JPG");
+  cv::Mat img = cv::imread("/home/pwolf/Downloads/akri_streaming_demo.png");
   cv::Mat rgb_channel[3];
   cv::split(img, rgb_channel);
   if (img.empty())
@@ -18,16 +17,24 @@ int main(int argc, char** argv)
   clone1 = green.clone();
   clone2 = green.clone();
   
-  cv::imshow("red", rgb_channel[0]);
-  cv::imshow("green", clone1);
-  cv::imshow("blue", rgb_channel[2]);
-
   double min, max;
   int minIdx, maxIdx;
   cv::minMaxIdx(green, &min, &max, &minIdx, &maxIdx);
 
   std::cout << "Min: " << minIdx << ", minvalue: " << min << std::endl;
   std::cout << "Max: " << maxIdx << ", maxValue: " << max << std::endl;
+
+  double thresh = (max - min) / 2.0;
+  clone1.setTo(thresh);
+  clone2.setTo(0);
+
+  cv::imshow("green", green);
+
+  cv::compare(green, clone1, clone2, cv::CMP_GE);
+
+  cv::subtract(green, thresh/2, green, clone2);
+
+  cv::imshow("green_compare", green);
 
   cv::waitKey(0);
 
