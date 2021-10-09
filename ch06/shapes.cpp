da6afc75cd00e05fbbb947e94002d3a768ae3a91
pwolf15 <patrick.wolf@live.com>
2021-10-09 16:57:34 -0700

    Added polylines

--- ch06/shapes.cpp
+++ ch06/shapes.cpp
@@ -20,12 +20,19 @@ int main(int argc, char** argv)
   cv::fillConvexPoly(img, points, 4, cv::Scalar(255, 255, 255));
 
   // fillPoly
+  std::vector<cv::Point> pts = {cv::Point(2,1), cv::Point(200,1), cv::Point(2,200), cv::Point(200,200) };
+  cv::fillPoly(img, pts,cv::Scalar(0,0,255));
 
   // line
+  cv::line(img, cv::Point(150, 1), cv::Point(150, 200), cv::Scalar(255, 255, 0));
 
-  // rectangle
+  // rectanglec
+  cv::rectangle(img, cv::Rect(20, 20, 200, 200), cv::Scalar(255, 255, 255), 20);
 
   // polyLines
+  // cv::Point 
+  int npts = 4;
+  cv::polylines(img, pts, true, cv::Scalar(0,0,0),2,150,0);
 
   cv::imshow("shapes", img);
 
