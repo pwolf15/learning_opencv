5189604c5b44ac3472d145eb1c623d59406b317a
pwolf15 <patrick.wolf@live.com>
2021-10-12 22:15:22 -0700

    Added more random examples

--- ch07/rng.cpp
+++ ch07/rng.cpp
@@ -34,5 +34,26 @@ int main(int argc, char** argv)
     std::cout << m2.at<float>(i,0) << "\n";
   } 
 
+  // unsigned uniform
+  cv::Mat m3(20,1,CV_8UC1);
+  rng.fill(m3, cv::RNG::UNIFORM, 0, 255);
+  std::cout << "20 unsigned bytes from 0 to 255 (uniform)" << std::endl;
+  for (int i = 0; i < m3.rows; ++i)
+  {
+    std::cout << (int)m3.at<uchar>(i,0) << "\n";
+  }
+
+  // 20 color triplets, each of three bytes uniform
+  cv::Mat m4(20,1,CV_8UC3);
+  rng.fill(m4, cv::RNG::UNIFORM, 0, 255);
+  std::cout << "20 color triplets from 0 to 255 (uniform)" << std::endl;
+  for (int i = 0 ; i < m4.rows; ++i)
+  {
+    cv::Vec3b color = m4.at<cv::Vec3b>(i,0);
+    std::cout << "r: " << (int)color[0] << " ";
+    std::cout << "g: " << (int)color[1] << " ";
+    std::cout << "b: " << (int)color[2] << "\n";
+  }
+
   return 0;
 }
