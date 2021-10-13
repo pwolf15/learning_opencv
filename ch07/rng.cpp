9c49d5ea8fc28359d14fd40ba229f6d93bab5303
pwolf15 <patrick.wolf@live.com>
2021-10-12 22:06:42 -0700

    RNG distribution (fill) examples

--- ch07/rng.cpp
+++ ch07/rng.cpp
@@ -14,5 +14,25 @@ int main(int argc, char** argv)
     std::cout << m.at<float>(i,0) << std::endl;
   }
 
+  // RNG arrays
+
+  // uniform
+  cv::Mat m1(20,1,CV_32F);
+  rng.fill(m1, cv::RNG::UNIFORM, 0.0, 1.0);
+  std::cout << "20 floating-point from 0.0 to 1.0 (uniform)" << std::endl;
+  for (int i = 0; i < m1.rows; ++i)
+  {
+    std::cout << m1.at<float>(i,0) << "\n";
+  }
+
+  // Gaussian
+  cv::Mat m2(20,1,CV_32F);
+  rng.fill(m2, cv::RNG::NORMAL, 0.0, 1.0);
+  std::cout << "20 floating point from 0.0 to 1.0 (gaussian)" << std::endl;
+  for (int i = 0; i < m2.rows; ++i)
+  {
+    std::cout << m2.at<float>(i,0) << "\n";
+  } 
+
   return 0;
 }
